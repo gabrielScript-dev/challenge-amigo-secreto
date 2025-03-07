5ea5743d73df39e950d09a84afadbf5dc554be0f
Gabriel Pereira <gabriel.script@gmail.com>
2025-03-07 12:58:02 -0300

    Adicionar funcionalidades de lista e sorteio de amigos

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const listaAmigos = [];
+const outListaAmigos = document.getElementById('listaAmigos');
+const outResultado = document.getElementById('resultado');
+
+function exibirListaAmigos(nome) {
+    outListaAmigos.innerHTML += `<li>${nome}</li>`;
+}
+
+function exibirResultado(nome) {
+    outListaAmigos.innerHTML = '';
+    outResultado.innerHTML = nome;
+}
+
+function adicionarAmigo() {
+    
+    const inAmigo = document.getElementById('amigo');
+    const nomeAmigo = inAmigo.value;
+
+    listaAmigos.push(nomeAmigo);
+    exibirListaAmigos(nomeAmigo);
+}
+
+function sortearAmigo() {
+    
+    const i = parseInt(Math.random() * listaAmigos.length);
+    const amigoSorteado = listaAmigos[i];
+
+    exibirResultado(amigoSorteado);
+}
